ed730ebbaa692383498449d0b130ac67492850c6
Dmitriy Vagner <haotic2015@gmail.com>
2022-04-10 22:30:40 +0300

    Созданы page object'ы и локаторы страниц main и login. Произведён рефакторинг кода с их применением. Добавлен тест перехода гостя на страницу с логином.

---
+++ pages/locators.py
@@ -0,0 +1,11 @@
+from selenium.webdriver.common.by import By
+
+
+class MainPageLocators():
+    LOGIN_LINK  = (By.CSS_SELECTOR, "#login_link")
+
+
+class LoginPageLocators():
+    LOGIN_FORM = (By.NAME, "login_submit")
+    REGISTER_FORM = (By.NAME, "registration_submit")
+
--- test_main_page.py
+++ test_main_page.py
@@ -1,15 +1,25 @@
 from .pages.main_page import MainPage
+from .pages.login_page import LoginPage
+
 
 link = "http://selenium1py.pythonanywhere.com/"
 
 
-def test_guest_can_go_to_login_page(browser): 
-    link = "http://selenium1py.pythonanywhere.com/catalogue/the-shellcoders-handbook_209?promo=midsummer" 
+def test_guest_can_go_to_login_page(browser):
+    link = "http://selenium1py.pythonanywhere.com"
     page = MainPage(browser, link)
     page.open()
     page.should_be_login_link()
 
-#def test_guest_can_go_to_login_page(browser):
-#   browser.get(link)
-#    login_link = browser.find_element_by_css_selector("#login_link")
-#    login_link.click()
+
+def test_guest_can_go_to_login_page(browser):
+    link = "http://selenium1py.pythoneverywhere.com"
+    page = MainPage(browser, link)
+    page.open()
+    page.page.go_to_login_page()
+    login_page = LoginPage(browser, browser.current_url)
+    login_page.shold_be_login_page()
+
+
+
+
